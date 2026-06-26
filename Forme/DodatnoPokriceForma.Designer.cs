faf4bc782b7b169a48a0c1787462dd06016133c8
Sofija <soffistef@gmail.com>
2026-06-26 16:40:38 +0200

    imam i ja nesto od sinocc

---
+++ Forme/DodatnoPokriceForma.Designer.cs
@@ -0,0 +1,49 @@
+﻿namespace Osiguranje.Forme
+{
+    partial class DodatnoPokriceForma
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // DodatnoPokriceForma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Salmon;
+            this.ClientSize = new System.Drawing.Size(1036, 391);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold);
+            this.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.Name = "DodatnoPokriceForma";
+            this.Text = "Dodatno Pokrice";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
